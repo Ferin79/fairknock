5bf14b5e5ae856f18df6eb8cc5604b557f024e91
Ferin <ferinpatel79@gmail.com>
2021-05-22 14:23:01 +0530

    Heroku Added

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -6,14 +6,16 @@ dotenv.config();
 
 module.exports = {
   type: process.env.DATABASE_TYPE,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
   entities: ["dist/**/*/model.js"],
   migrations: ["dist/migration/**/*.migration.js"],
   subscribers: ["dist/subscriber/**/*.subscriber.js"],
 };
+
+// host: process.env.DATABASE_HOST,
+// port: process.env.DATABASE_PORT,
+// username: process.env.DATABASE_USERNAME,
+// password: process.env.DATABASE_PASSWORD,
+// database: process.env.DATABASE_NAME,
